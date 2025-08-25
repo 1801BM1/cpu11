d1f5122cfe7177f87f425cec7ba7ae6e19c75eb4
Yuriy Shestakov <182526021+shisha-gh@users.noreply.github.com>
2025-08-25 12:20:23 +0000

    Docker: Added binutils with target=pdp11-aout
    Signed-off-by: Yuriy Shestakov <182526021+shisha-gh@users.noreply.github.com>

--- docker/run_noble.sh
+++ docker/run_noble.sh
@@ -5,5 +5,5 @@ podman run --rm -ti  \
 	-v "$ROOT:/work" \
 	-e "EXT_ROOT=$ROOT" \
 	-e "WD=$WD" \
-	quay.io/yshestakov/cpu11-tools:latest  \
+	quay.io/yshestakov/cpu11-tools-noble:latest  \
 	/bin/bash "$@"
